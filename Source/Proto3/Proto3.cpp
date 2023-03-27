6b328507c5b346c04f0e57994259a8208efbd3cb
trandunz <54924605+trandunz@users.noreply.github.com>
2023-03-27 19:40:09 +1300

    Project Base

---
+++ Source/Proto3/Proto3.cpp
@@ -0,0 +1,7 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Proto3.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Proto3, "Proto3" );
+ 
---
+++ Source/Proto3/Proto3GameMode.cpp
@@ -0,0 +1,15 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Proto3GameMode.h"
+#include "Proto3Character.h"
+#include "UObject/ConstructorHelpers.h"
+
+AProto3GameMode::AProto3GameMode()
+{
+	// set default pawn class to our Blueprinted character
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/ThirdPerson/Blueprints/BP_ThirdPersonCharacter"));
+	if (PlayerPawnBPClass.Class != NULL)
+	{
+		DefaultPawnClass = PlayerPawnBPClass.Class;
+	}
+}
