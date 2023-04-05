bd1acda51d78162db67f0328b390bb54d88c9acf
trandunz <54924605+trandunz@users.noreply.github.com>
2023-04-05 17:12:20 +1200

    Setup dying and fade to black + respawn

--- Source/Proto3/Widgets/GameHUD.h
+++ Source/Proto3/Widgets/GameHUD.h
@@ -14,13 +14,28 @@ class PROTO3_API UGameHUD : public UUserWidget
 {
 	GENERATED_BODY()
 
+	virtual void NativeTick(const FGeometry& MyGeometry, float InDeltaTime) override;
 
 public:
 	UPROPERTY(VisibleAnywhere, meta=(BindWidget))
 	class UWidget_PauseMenu* PauseMenu;
 
+	UPROPERTY(VisibleAnywhere, meta=(BindWidget))
+	class UImage* FadeImage;
 public:
 	UFUNCTION(BlueprintCallable)
 	void EnableDisableMenu();
+
+	UFUNCTION(BlueprintCallable)
+	void FadeIn(float _time);
 	
+	UFUNCTION(BlueprintCallable)
+	void FadeOut(float _time);
+
+	UPROPERTY(VisibleAnywhere)
+	float FadeInTimer{};
+	UPROPERTY(VisibleAnywhere)
+	float FadeOutTimer{};
+
+	float LatestFadeTime{};
 };
